813dce340a17426b25ee0c99ee0439359d7eb091
Paskan <radialvector@outlook.com>
2026-02-11 16:40:19 +0530

    Update SectionRenderer.jsx

--- frontend/src/pages/Add pages/Cms/SectionRenderer.jsx
+++ frontend/src/pages/Add pages/Cms/SectionRenderer.jsx
@@ -1080,7 +1080,7 @@ function BooksSection({ content }) {
           ))}
         </Swiper>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-6 justify-center">
+        <div className="grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-6 justify-center">
           {latestBooks.map((book) => (
             <BookCard key={book._id} book={book} />
           ))}
